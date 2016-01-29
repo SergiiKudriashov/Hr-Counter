4a8f41581568c561f4529762d5a0fec450a398be
SergiiKudriashov <Sergii_Kudriashov@epam.com>
2016-01-29 08:17:40 +0200

    0007 generating select

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,19 @@
 window.onload = function(){
 	var pumpHolder = document.getElementById('pump-holder');
-	pumpHolder.innerHTML = '<select> </select>';
+	pumpHolder.innerHTML = '<select></select>';
 	var selectPump = pumpHolder.getElementsByTagName('select');
+		selectPump.inner = ''; 
 	for (var key in pumps){
 		var addInner = selectPump.innerHTML;
-		var a = key;
-		console.log(a);
-			// selectPump.innerHTML += '<option> One' + pumps.key.type + '</option>';
-			selectPump.innerHTML += '<option> One' + '</option>';
+			selectPump.innerHTML = addInner + '<option> One' + '</option>';
 	};
 };
+function pumpGenerate() {
+ var pumpHolder = document.getElementById('pump-holder');
+	pumpHolder.innerHTML = '<select></select>';
+	var selectPump = pumpHolder.getElementsByTagName('select');
+			for (var key in pumps){
+		var addInner = selectPump.innerHTML;
+			selectPump.innerHTML = addInner + '<option> One' + '</option>';
+	};
+}
